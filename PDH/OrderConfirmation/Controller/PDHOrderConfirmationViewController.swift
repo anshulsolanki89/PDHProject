c16e7fb742705a599f90e7137f159becd4a8c9a2
Anshul Solanki <asolanki@sapient.com>
2015-12-15 00:51:57 +0530

    PDH Started working on order confirmation screen.

---
+++ PDH/OrderConfirmation/Controller/PDHOrderConfirmationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PDHOrderConfirmationViewController.swift
+//  PDH
+//
+//  Created by Anshul Solanki on 12/15/15.
+//  Copyright © 2015 Anshul Solanki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PDHOrderConfirmationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    deinit {
+        print("\(self) DEALLOCATED")
+    }
+}
---
+++ PDH/OrderConfirmation/Views/PDHOrderConfirmationView.swift
@@ -0,0 +1,17 @@
+//
+//  PDHOrderConfirmationView.swift
+//  PDH
+//
+//  Created by Anshul Solanki on 12/15/15.
+//  Copyright © 2015 Anshul Solanki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PDHOrderConfirmationView: PDHView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
